356f1ec1383f3914d9199c486d16ae338b83ad5b
tanselberkant <tanselberkant@gmail.com>
2020-09-20 02:11:28 +0300

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { NavbarComponent } from './front/navbar/navbar.component';
+import { TeamsComponent } from './front/teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',redirectTo:'/front',pathMatch :'full'
+  },
+  {
+    path : 'front',component:NavbarComponent,children : [
+      {path : "teams",component:TeamsComponent}
+    ]
+  },
+  {
+    path : 'admin',component : DashboardComponent,children:[]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './front/navbar/navbar.component';
+import { NotificationComponent } from './front/notification/notification.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { LoginComponent } from './admin/login/login.component';
+import { TeamsComponent } from './front/teams/teams.component';
+import { AboutComponent } from './front/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NotificationComponent,
+    DashboardComponent,
+    LoginComponent,
+    TeamsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
